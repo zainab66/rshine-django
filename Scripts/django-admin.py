9353197b1ad5c1d5d330802c0b28d050a9ad87c3
zainab66 <zainab66r@yahoo.com>
2020-04-22 19:48:37 -0400

    Django 3.0.5 installed

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mou\desktop\rshine_project\2020\rshine_site\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
